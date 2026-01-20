a98ef70f0ef1d71f4c9d0c3b4c1678eed18c8114
Jonatan Machado <jonatanmachado35@gmail.com>
2026-01-20 16:16:13 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,16 @@ async function bootstrap() {
       transform: true
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Imobix API')
+    .setDescription('Imobix API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port);
 }
